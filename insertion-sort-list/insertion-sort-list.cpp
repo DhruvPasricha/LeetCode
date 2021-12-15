9fdc4d832ccf91dc507465b951af68e788e58f16
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-12-15 12:15:32 +0530

    Time: 62 ms (17.18%), Space: 9.8 MB (11.40%) - LeetHub

--- insertion-sort-list/insertion-sort-list.cpp
+++ insertion-sort-list/insertion-sort-list.cpp
@@ -1,28 +1,81 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* insertionSortList(ListNode* head) {
-        
-        if(head == 0) return 0;
-        
-        head -> next = insertionSortList(head -> next); 
+    
+    // inserts the 1st element from unsortedLL into the sortedLL such that sortedLL remains sorted
+    ListNode* insert(ListNode* sortedHead, ListNode* unsortedHead){
 
-        ListNode* current = head;
+        ListNode *current = sortedHead;
+        ListNode* prev = NULL;
         
-        while(current -> next != 0 and current -> next -> val < current -> val){
-            swap(current -> val, current -> next -> val);
+        while(current != 0 and current -> val < unsortedHead -> val){
+            prev = current;
             current = current -> next;
         }
         
+        if(prev == 0){
+            unsortedHead -> next = sortedHead;
+            sortedHead = unsortedHead;
+        }else{
+            prev -> next = unsortedHead;
+            unsortedHead -> next = current;
+        }
+        return sortedHead;
+    }
+    
+    
+    ListNode* solveItr(ListNode* head) {
+        
+        // empty list
+        if(head == 0) return 0; 
+        
+        // head : a0 -> a1 -> a2 ......
+        
+        // breaking the connection between a0 and a1
+        ListNode* remaining = head -> next;
+        head -> next = 0;
+        
+        // head : a0
+        // remaining : a1 -> a2 -> a3 ...........
+
+        // insert the remaining list nodes one by one in the head in sorted order 
+        while(remaining != 0){ 
+            ListNode *next = remaining -> next;
+            head = insert(head, remaining);
+            remaining = next;
+        }
+        
+        // list is sorted
+        return head;
+    }
+    
+    
+    
+    ListNode* solveRec(ListNode* head) {
+        
+        // empty list
+        if(head == 0) return 0; 
+        
+        // sorts the list from head->next till the end
+        ListNode *smallSortedList = insertionSortList(head -> next); 
+        
+        // list is now sorted from head->next till the end
+        
+        // break the connection between the two
+        head -> next = 0;
+        
+        // smallSortedList : a1 -> a2 -> a3 -> a4 ........
+        // head : a0
+        
+        // insert head in the above smallSortedList
+        head = insert(smallSortedList, head);
+        
+        // list is sorted
+        return head;
+    }
+    
+    ListNode* insertionSortList(ListNode* head) {
+        head = solveRec(head);
+        // head = solveItr(head);       
         return head;
     }
 };
