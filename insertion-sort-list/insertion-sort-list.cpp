873735e7457d64517d605294eb1d1915605c8a9e
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-12-15 12:26:47 +0530

    Time: 48 ms (43.85%), Space: 9.9 MB (11.40%) - LeetHub

--- insertion-sort-list/insertion-sort-list.cpp
+++ insertion-sort-list/insertion-sort-list.cpp
@@ -22,8 +22,59 @@ public:
         return sortedHead;
     }
     
+    ListNode* solveRec(ListNode* head) {
+        
+        // empty list
+        if(head == 0) return 0; 
+        
+        // sorts the list from head->next till the end
+        ListNode *smallSortedList = insertionSortList(head -> next); 
+        
+        // list is now sorted from head->next till the end
+        
+        // break the connection between the two
+        head -> next = 0;
+        
+        // smallSortedList : a1 -> a2 -> a3 -> a4 ........
+        // head : a0
+        
+        // insert head in the above smallSortedList
+        head = insert(smallSortedList, head);
+        
+        // list is sorted
+        return head;
+    }
+    
     
     ListNode* solveItr(ListNode* head) {
+        
+        // empty list
+        if(head == 0) return 0;
+        
+        stack<ListNode*> st;
+        
+        // head : a0 -> a1 -> a2 ...... -> an-1
+        
+        // push first n - 1 nodes into the stack
+        while(head -> next != 0){
+            st.push(head);
+            head = head -> next;
+        }
+        
+        // head : an 
+        // st (remaining) : an-1 , an-2 .............. , a0  
+        
+        // insert the remaining list nodes one by one in the head in sorted order 
+        while(not st.empty()) {
+            head = insert(head, st.top());
+            st.pop();
+        }
+        
+        return head;
+        
+    }
+    
+    ListNode* solveItr2(ListNode* head) {
         
         // empty list
         if(head == 0) return 0; 
@@ -48,34 +99,11 @@ public:
         return head;
     }
     
-    
-    
-    ListNode* solveRec(ListNode* head) {
-        
-        // empty list
-        if(head == 0) return 0; 
-        
-        // sorts the list from head->next till the end
-        ListNode *smallSortedList = insertionSortList(head -> next); 
-        
-        // list is now sorted from head->next till the end
-        
-        // break the connection between the two
-        head -> next = 0;
-        
-        // smallSortedList : a1 -> a2 -> a3 -> a4 ........
-        // head : a0
-        
-        // insert head in the above smallSortedList
-        head = insert(smallSortedList, head);
-        
-        // list is sorted
-        return head;
-    }
-    
+
     ListNode* insertionSortList(ListNode* head) {
-        head = solveRec(head);
-        // head = solveItr(head);       
+        // head = solveRec(head);
+        head = solveItr(head);
+        // head = solveItr2(head);       
         return head;
     }
 };
