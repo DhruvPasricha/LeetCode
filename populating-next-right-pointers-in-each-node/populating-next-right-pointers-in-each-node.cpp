82aef940d9b8a59647d21024dbb8cbfffb428875
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2021-12-30 02:34:18 +0530

    Time: 29 ms (10.10%), Space: 16.8 MB (84.65%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -1,47 +1,14 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
+    void connect(Node *&src, Node *&dest) {
+        src -> next = dest;
+    }
 public:
     Node* connect(Node* root) {
-        
-        if(root == NULL) return NULL;
-        queue<Node*> q;
-        
-        Node *prev = NULL; 
-        q.push(root);
-        q.push(NULL);
-        
-        while(q.size() > 0) {
-            
-            auto f = q.front();
-            q.pop();
-            
-            if(f == NULL) {
-                if(q.size() > 0) q.push(NULL);
-                else break;
-            } else { 
-                if(prev != NULL) prev -> next = f;
-                if(f->left) q.push(f->left);
-                if(f->right) q.push(f->right);
-            }
-            prev = f;
-        }
+        if(root == NULL or root -> left == NULL) return root;
+        if(root -> left) connect(root -> left, root -> right);
+        if(root -> next) connect(root -> right, root -> next -> left);
+        root->left = connect(root -> left);
+        root->right = connect(root -> right);
         return root;
     }
 };
