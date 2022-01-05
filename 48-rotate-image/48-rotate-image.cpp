e3c59d002b9ad0bad833369895bd52885214c04a
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-05 13:08:50 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (30.94%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,8 +1,7 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        
-        int n = matrix.size();
+class Solution { 
+    
+    void solve1(vector<vector<int>> &matrix){
+         int n = matrix.size();
         
         for(int i = 0 ;i < n;i++) 
             for(int j = 0;j < n;j++)
@@ -16,6 +15,22 @@ public:
             for(int j = 0;j < n;j++)
                 matrix[i][j] = (matrix[i][j]/2001) - 1000;
         
+    }
+    
+    void solve2(vector<vector<int>> &matrix) {
         
+        int n = matrix.size();
+        
+        for(int i = 0;i < n;i++) 
+            reverse(matrix[i].begin(), matrix[i].end());
+        
+        for(int i = 0;i < n;i++)
+            for(int j = 0;j < n - 1 - i;j++)
+                swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+        
+    }
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        solve2(matrix);
     }
 };
