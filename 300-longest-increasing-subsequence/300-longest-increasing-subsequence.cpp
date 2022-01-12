fd4d1c893241e340f6bd738291cabdc093c1ad4f
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-12 19:16:11 +0530

    Time: 49 ms (63.25%), Space: 12.5 MB (6.00%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,18 +1,39 @@
 class Solution {
+    map<int,int> mp;
+    int ans = 0;
+    void insert(int x) {
+        
+        if(mp.count(x)) return;
+        
+        auto itr = mp.upper_bound(x);
+        
+        if(itr == mp.end()) {
+            itr--;
+            mp[x] = itr->second + 1;   
+        } else {
+            int cur = itr->second;
+            mp.erase(itr);
+            mp[x] = cur;
+        }
+        ans = max(ans, mp[x]);
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
+        mp[INT_MIN] = 0;
         vector<int> LIS;
         int n = nums.size();
         for(int i = 0;i < n;i++) {
-            
-            if(LIS.empty() or LIS.back() < nums[i]) {
-                LIS.push_back(nums[i]);
-            } else {
-                auto itr = lower_bound(LIS.begin(), LIS.end(), nums[i]);
-                *itr = nums[i];
-            }
+            insert(nums[i]);
         }
-        return LIS.size();
+        return ans;
     }
 };
+
+// 1 2 -1 10 100
+
+
+
+// 1 -> 1
+// 2 -> 2
+
+// -1 -> 1
