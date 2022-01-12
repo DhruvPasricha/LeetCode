3564a0dc7e1dbf4143b349d9af86cc863c8ed453
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-12 15:34:49 +0530

    Time: 0 ms (100.00%), Space: 10 MB (63.53%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,26 +1,30 @@
 class Solution {
+    
+    bool possible(vector<int> &A, int idx) {
+        int prev = INT_MIN;
+        int n = A.size();
+        for(int i = 0;i < n;i++) {
+            if(i == idx) continue;
+            if(A[i] <= prev) return false;
+            prev = A[i];
+        }
+        return true;
+    }
+    
 public:
     bool canBeIncreasing(vector<int>& nums) {
         
-      int n = nums.size(); 
-      for(int i = 0;i < n;i++) {
-           int prev = INT_MIN;
-           bool ok = true;
-           for(int j = 0;j < n;j++) {
-               if(i == j) continue;
-               if(nums[j] <= prev) {
-                   ok = false;
-                   break;
-               }
-               prev = nums[j];
-          }
-          
-         if(ok){ 
-              cout << "removed" << i << endl;
-              return true;
+        int n = nums.size();
+        int choice1 = -1;
+        int choice2 = -1;
+        for(int i = 1;i < n;i++) {
+            
+            if(nums[i] <= nums[i - 1]) {
+                choice1 = i - 1;
+                choice2 = i;
+                break;
+            }
         }
-       }
-        return false;
+        return possible(nums, choice1) or possible(nums, choice2);
     }
 };
-
