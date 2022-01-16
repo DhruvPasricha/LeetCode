99731b0e1e759e0febb0f30fcf3d94bd50427360
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-16 16:01:38 +0530

    Time: 33 ms (7.07%), Space: 17.1 MB (32.93%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,26 +1,17 @@
 class Solution {
-    vector<int> pre;
-    vector<int> suf;
 public:
     int maxDistToClosest(vector<int>& seats) {
         int n = seats.size();
         
-        pre.assign(n, -1e6);
-        suf.assign(n, 1e6);
-        
-        for(int i = 0;i < n;i++) {
-            if(seats[i] == 1) {
-                pre[i] = i;
-            } else if(i > 0) {
-                pre[i] = pre[i-1];
-            }
-        }
+        int prev = -1e6;
         
         for(int i = n - 1;i >= 0;i--) {
             if(seats[i] == 1) {
-                suf[i] = i;
+                seats[i] = i;
             } else if(i < n - 1) {
-                suf[i] = suf[i+1];
+                seats[i] = seats[i + 1];
+            } else {
+                seats[i] = 1e6;
             }
         }
         
@@ -28,13 +19,16 @@ public:
         
         for(int i = 0;i < n;i++) {
             
-            if(seats[i] == 1) continue;
-            
-            int l = i - pre[i];
-            int r = suf[i] - i;
+            if(seats[i] == i){ 
+                prev = i;
+                continue;
+            }
+            int l = i - prev;
+            int r = seats[i] - i;
             
             ans = max(ans, min(l, r));
         }
         return ans;
     }
 };
+
