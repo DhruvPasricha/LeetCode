0c3d9e87a558fd28fc475bd3fd47c01bf5093eda
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-20 12:35:29 +0530

    Time: 82 ms (18.92%), Space: 18.9 MB (16.35%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    
+    inline int Ceil(int num, int den)  {
+        return (num / den) + (num % den != 0);
+    }
+
+    bool good(vector<int> &piles, int k, int h)  {
+        
+        int hoursReq = 0;
+        
+        for(int &pile : piles) {
+            hoursReq += Ceil(pile, k);    
+        }
+        
+        return hoursReq <= h;
+    }
+    
+    
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int l = 0;
+        int r = 1e9;
+        
+        while(r - l > 1) {
+            
+            int m = (l + r) >> 1;
+            
+            if(good(piles, m, h))
+                r = m;
+            else
+                l = m;
+        }
+        
+        return r;
+    }
+};
