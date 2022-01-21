3546a3e814978e6f33ffe800a07db46df3e90711
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-21 19:29:32 +0530

    Time: 92 ms (28.71%), Space: 49.8 MB (83.67%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,50 +1,75 @@
 class Solution {
     
     vector<int> prefix, prefixMin, suffixMin;
+    int mod = 1e4 + 1;
     
-    void init(vector<int> &gas, vector<int> &cost) {
+    int init(vector<int> &gas, vector<int> &cost) {
           
         int n = gas.size();
         
-        prefix.assign(n, 0);
-        prefix[0] = gas[0] - cost[0]; 
-        for(int i = 1;i < n;i++) 
-            prefix[i] = prefix[i - 1] + (gas[i] - cost[i]);
+        // cost[i] = [gas[i] * (mod)]  + cost[i]; 
+        for(int i = 0;i < n;i++) 
+            cost[i] += (mod * gas[i]);
         
-        prefixMin.assign(n, 0);
-        prefixMin[0] = prefix[0];
-        for(int i = 1;i < n;i++) 
-            prefixMin[i] = min(prefixMin[i - 1], prefix[i]);
+        // converting gas[i] to prefix Array
+        gas[0] = gas[0] - cost[0] % mod;
+        for(int i = 1;i < n;i++)
+            gas[i] = gas[i - 1] + (gas[i] - (cost[i] % mod));
         
+        int total = gas[n - 1]; 
         
-        suffixMin.assign(n, 0);
-        suffixMin[n - 1] = prefix[n - 1];
+        // converting gas[i] to suffixMin Array 
         for(int i  = n - 2;i >= 0;i--) 
-            suffixMin[i] = min(suffixMin[i + 1], prefix[i]);
+            gas[i] = min(gas[i], gas[i + 1]);
         
+        return total;
+    }
+    
+    
+    void recover(vector<int> &gas, vector<int> &cost) {
+        
+        int n = gas.size();
+        
+        for(int i = 0;i < n;i++) {
+            gas[i] = cost[i] / mod;
+            cost[i] %= mod;
+        }
     }
     
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-      
-        init(gas, cost);
         
         int n = gas.size();
         
+        int total = init(gas, cost);
+        vector<int> &suffixMin = gas;
+        
+        int prefix = 0;
+        int prefixMin = INT_MAX; 
+        int ans = -1;
+        
         for(int i = 0; i < n; i++) {
             
-            int rightExtra = (i == 0) ? 0 : prefix[i - 1];  
-            int leftRemaining = prefix[n - 1] - rightExtra; 
+            int rightExtra = (i == 0) ? 0 : prefix;  
+            int leftRemaining = total - rightExtra;
             
-            int leftMin = (i == 0) ? 0 : prefixMin[i - 1] + leftRemaining;
-            int rightMin = suffixMin[i] - rightExtra;
+            int leftMin = (i == 0) ? 0 : prefixMin + leftRemaining;
+            int rightMin = suffixMin[i] - rightExtra; 
             
             int pathMin = min(leftMin, rightMin);
             
-            if(pathMin >= 0) 
-                return i;
+            if(pathMin >= 0) {
+                ans = i;
+                break;
+            }
+            
+            prefix += ((cost[i] / mod) - (cost[i] % mod));
+            prefixMin = min(prefixMin, prefix);
+            
         }
         
-        return -1;
+        recover(gas, cost);
+        
+        return ans;
     }
 };
