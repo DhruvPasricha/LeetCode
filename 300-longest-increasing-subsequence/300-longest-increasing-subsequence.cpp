c06dd41afcc85e360498746c20a61c42a9050f8b
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-23 09:51:33 +0530

    Time: 12 ms (76.86%), Space: 10.6 MB (36.92%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,39 +1,16 @@
 class Solution {
-    map<int,int> mp;
-    int ans = 0;
-    void insert(int x) {
-        
-        if(mp.count(x)) return;
-        
-        auto itr = mp.upper_bound(x);
-        
-        if(itr == mp.end()) {
-            itr--;
-            mp[x] = itr->second + 1;   
-        } else {
-            int cur = itr->second;
-            mp.erase(itr);
-            mp[x] = cur;
-        }
-        ans = max(ans, mp[x]);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        mp[INT_MIN] = 0;
-        vector<int> LIS;
         int n = nums.size();
-        for(int i = 0;i < n;i++) {
-            insert(nums[i]);
+        // dp[i] = min element at which an increasing subsequence ends having length = i
+        vector<int> dp(n + 1, INT_MAX);         
+        dp[0] = INT_MIN;
+        int ans =  0;
+        for(int &num : nums) {
+            int pos = lower_bound(dp.begin(), dp.end(), num) - dp.begin();
+            ans = max(ans, pos);
+            dp[pos] = num;
         }
         return ans;
     }
 };
-
-// 1 2 -1 10 100
-
-
-
-// 1 -> 1
-// 2 -> 2
-
-// -1 -> 1
