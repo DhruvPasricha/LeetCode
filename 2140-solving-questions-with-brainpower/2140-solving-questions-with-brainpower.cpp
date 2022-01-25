80e133b2ad980f1eaf618b13a6c7735763120766
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-25 20:34:48 +0530

    Time: 578 ms (45.25%), Space: 115.4 MB (57.79%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,30 +1,26 @@
-
 class Solution {
-    #define v vector
-    v<int64_t> dp;
-    
-    int64_t solve(v<v<int>> &A, int pos) {
-        
-        if(pos >= A.size()) 
-            return 0;
-        
-        if(dp[pos] != -1)
-            return dp[pos];
-        
-        int64_t ans1 = solve(A, pos + 1);
-        int64_t ans2 = (int64_t)(A[pos][0]) + solve(A, pos + 1 + A[pos][1]);
-        
-        return dp[pos] = max(ans1, ans2);
-        
-    }
-    
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         
-        dp.assign(questions.size(), -1);
+        int64_t n = questions.size();
         
-        int64_t ans = solve(questions, 0);
+        vector<int64_t> dp(n + 1);
         
-        return ans;
+        for(int i = n - 1;i >= 0;i--) {
+            
+            // skipped
+            int64_t ans1 = dp[i + 1];
+            
+            // solved
+            int64_t points = questions[i][0];
+            int64_t next = (i + 1 + questions[i][1]);
+            next = min(n, next);
+            int64_t ans2 = points + dp[next];
+            
+            // result
+            dp[i] = max(ans1, ans2);
+        }
+        
+        return dp[0];
     }
 };
