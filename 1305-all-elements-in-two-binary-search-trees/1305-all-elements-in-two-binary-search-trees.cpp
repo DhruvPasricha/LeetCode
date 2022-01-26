d3ad75f5b6b0c774e9528e7a83cdc1e180b10496
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-26 16:38:20 +0530

    Time: 258 ms (20.59%), Space: 86.8 MB (15.36%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    
+    void merge(vector<int> &A, vector<int> &B, vector<int> &Ans) {
+        
+        int i = 0;
+        int n = A.size();
+        int j = 0;
+        int m = B.size();
+        
+        while(i < n or j < m) {
+            int option1 = (i < n) ? A[i] : INT_MAX;
+            int option2 = (j < m) ? B[j] : INT_MAX;
+            
+            if(option1 <= option2)
+                Ans.push_back(option1), i++;
+            else
+                Ans.push_back(option2), j++;
+        } 
+        
+        
+    }
+    
+    
+    
+    void getInorder(TreeNode* root, vector<int> &Ans) {
+        
+        if(root == NULL)
+            return;
+        
+        getInorder(root->left, Ans);
+        Ans.push_back(root->val);
+        getInorder(root->right, Ans);
+    }
+    
+    
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        
+        vector<int> tree1;
+        getInorder(root1, tree1);
+        
+        vector<int> tree2;
+        getInorder(root2, tree2);
+        
+        vector<int> Ans;
+        merge(tree1, tree2, Ans);
+        
+        return Ans;
+    }
+};
