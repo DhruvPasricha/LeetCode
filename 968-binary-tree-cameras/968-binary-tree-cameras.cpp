0dd62e3315c11df382dce727a79926e8c8b75535
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-01-28 05:27:43 +0530

    Time: 59 ms (5.28%), Space: 27.8 MB (5.80%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    map<TreeNode*, int> dp[2][2];
+    
+    int64_t solve(TreeNode* root, bool mandatory, bool covered) {
+        
+        if(root == NULL) {
+            if(mandatory) return INT_MAX;
+            return 0;
+        }
+        
+        if(dp[mandatory][covered].count(root)) 
+            return dp[mandatory][covered][root];
+        
+        // kept here
+        int64_t ans = 1 + solve(root->left, false, true) + solve(root->right, false, true);
+        
+        
+        // skipped
+        if(not mandatory) {
+            
+            int64_t ans1 = solve(root->left, not covered, false) + solve(root->right, false, false);
+            int64_t ans2 = solve(root->left, false, false) + solve(root->right, not covered, false);
+            
+            ans = min({ans, ans1, ans2});
+        }
+        
+        return dp[mandatory][covered][root] = ans;   
+    }
+    
+public:
+    int minCameraCover(TreeNode* root) {
+         
+        return solve(root, false, false);
+    }
+};
