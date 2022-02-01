0696734e5bba6d7805f1eac50a2fc10d410620f4
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-02-02 02:39:35 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (33.49%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,36 +1,17 @@
-class Solution { 
-    // works only when matrix[i][j] is not too big
-    void solve1(vector<vector<int>> &matrix){
-         int n = matrix.size();
-        
-        for(int i = 0 ;i < n;i++) 
-            for(int j = 0;j < n;j++)
-                matrix[i][j] += 1000;
-        
-        for(int i = 0;i < n;i++)
-            for(int j = 0;j < n;j++)
-                matrix[j][n - i - 1] += 2001*(matrix[i][j] % 2001);
-        
-        for(int i = 0;i < n;i++)
-            for(int j = 0;j < n;j++)
-                matrix[i][j] = (matrix[i][j]/2001) - 1000;
-        
-    }
-    
-    void solve2(vector<vector<int>> &matrix) {
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
         
-        for(int i = 0;i < n;i++) 
+        for(int i = 0; i < n; i++) {
             reverse(matrix[i].begin(), matrix[i].end());
+        }
         
-        for(int i = 0;i < n;i++)
-            for(int j = 0;j < n - 1 - i;j++)
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < n - i;j++) {
                 swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
-        
-    }
-public:
-    void rotate(vector<vector<int>>& matrix) {
-        solve2(matrix);
+            }
+        }
     }
 };
