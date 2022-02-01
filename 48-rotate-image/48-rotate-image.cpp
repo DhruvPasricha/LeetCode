d828dbd5e87ebf72d9e2e9de89b4bb316c38d830
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-02-02 03:00:05 +0530

    Time: 11 ms (6.75%), Space: 6.9 MB (92.97%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,17 +1,43 @@
 class Solution {
+    
+    void rotate(int &a, int &b, int &c, int &d) {
+        int temp = a;
+        a = d;
+        d = c;
+        c = b;
+        b = temp;
+    }
+    
 public:
     void rotate(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
         
-        for(int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
+        int startCol = 0;
+        int startRow = 0;
+        int endCol = n - 1;
+        int endRow = n - 1;
         
-        for(int i = 0;i < n;i++) {
-            for(int j = 0;j < n - i;j++) {
-                swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+        
+        while(startRow < endRow and startCol < endCol) {
+            
+            int m = endRow - startRow + 1;
+            
+            
+            for(int i = 0;i < m - 1; i++) {
+                
+                int &element1 = matrix[startRow][startCol + i];
+                int &element2 = matrix[startRow + i][endCol];
+                int &element3 = matrix[endRow][endCol - i];
+                int &element4 = matrix[endRow - i][startCol];
+                
+                rotate(element1, element2, element3, element4);
             }
+            
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
         }
     }
 };
