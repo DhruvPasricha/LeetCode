5b58a48de7df04337da101134b838e136c71d514
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-02-03 05:41:03 +0530

    Time: 368 ms (35.56%), Space: 27.7 MB (9.26%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,14 +1,10 @@
 class Solution {
     
-    vector<int> getAllPairSum(vector<int> &A, vector<int> &B) {
-        
-        vector<int> Ans;
+    void generateAllPairSum(vector<int> &A, vector<int> &B, unordered_map<int, int> &freq) {
         
         for(int &a : A)
             for(int &b : B)
-                Ans.push_back(a + b);
-        
-        return Ans;
+                freq[a + b]++;
         
     }
     
@@ -17,16 +13,16 @@ public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         
         
-        vector<int> A = getAllPairSum(nums1, nums2);
-        vector<int> B = getAllPairSum(nums3, nums4);
+        unordered_map<int,int> A;
+        unordered_map<int,int> B;
         
-        unordered_map<int, int> mp;
-        
-        for(int &b : B) mp[b]++;
+        generateAllPairSum(nums1, nums2, A);
+        generateAllPairSum(nums3, nums4, B);
         
         int ans = 0;
         
-        for(int &a : A) ans += mp[-a];
+        for(auto &a : A) 
+            ans += (a.second * B[-a.first]);
         
         return ans;
     }
