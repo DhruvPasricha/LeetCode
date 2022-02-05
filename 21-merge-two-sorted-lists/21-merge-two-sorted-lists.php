3200bbcd16b05422f073de92de98392b67b11977
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-02-06 02:55:21 +0530

    Time: 0 ms (100.00%), Space: 19.4 MB (7.54%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.php
@@ -0,0 +1,31 @@
+class Solution {
+
+     function insert(&$x, &$head, &$tail) {
+        
+        if($head == null) $head = $x;
+        else $tail->next = $x;
+        
+        $tail = $x;
+        $x = $x->next;
+        
+    }
+    
+    function mergeTwoLists($list1, $list2) {
+        if($list1 == null) return $list2;
+        if($list2 == null) return $list1;
+        
+        $head = null;
+        $tail = null;
+        
+        while($list1 and $list2) {
+            if($list1->val <= $list2->val) Solution::insert($list1, $head, $tail);
+            else Solution::insert($list2, $head, $tail);
+        }
+        
+        if($list1 == null) $tail->next = $list2;
+        if($list2 == null) $tail->next = $list1;
+        
+        return $head;
+        
+    }
+}
