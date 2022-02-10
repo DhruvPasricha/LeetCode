fe7a7bd78d0b0717dc2a36393fda93a58afff4f4
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-02-10 08:35:54 +0530

    Time: 46 ms (83.20%), Space: 11.8 MB (8.94%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,17 +1,51 @@
 class Solution {
+   
+    void calculate(vector<int> &nums, int start, int end, vector<int> &Ans, function<bool(int, int)> f) {
+        
+        int n = nums.size();
+        int step = 1;
+        int defaultValue = -1;
+        int finalValue = n;
+        if(start > end) {
+            step = -1;
+            defaultValue = n;
+            finalValue = -1;
+        }
+        Ans.assign(n, defaultValue);
+        stack<int> st;
+        for(int i  = start; i != finalValue; i += step) {
+            
+            while(st.size() > 0 and f(nums[st.top()], nums[i])) st.pop();
+            
+            if(st.size() > 0) Ans[i] = st.top();
+            st.push(i);
+        }
+    }
+    
+    
 public:
     long long subArrayRanges(vector<int>& nums) {
+        
         int n = nums.size();
+        
+        vector<int> prevSmaller, prevGreater;
+        vector<int> nextSmallerOrEqual, nextGreaterOrEqual;
+    
+        calculate(nums, 0, n - 1, prevSmaller, [](int a, int b)->bool {return a >= b;});
+        calculate(nums, 0, n - 1, prevGreater, [](int a, int b)->bool {return a <= b;});
+        calculate(nums, n - 1, 0, nextSmallerOrEqual, [](int a, int b)->bool {return a > b;});
+        calculate(nums, n - 1, 0, nextGreaterOrEqual, [](int a, int b)->bool {return a < b;});
+        
         int64_t ans = 0;
-        for(int i = 0;i < n;i++) {
-            int mx = nums[i];
-            int mn = nums[i];
-            for(int j = i;j >= 0;j--) {
-                mx = max(nums[j], mx);
-                mn = min(nums[j], mn);
-                ans += (int64_t)(mx - mn);
-            }
+        for(int i = 0;i < n; i++) {
+            
+            int64_t minContribution = (int64_t)(i - prevSmaller[i]) * (int64_t)(nextSmallerOrEqual[i] - i);
+            int64_t maxContribution = (int64_t)(i - prevGreater[i]) * (int64_t)(nextGreaterOrEqual[i] - i);
+            
+            int64_t netContribution = (maxContribution - minContribution) * nums[i];
+            ans += netContribution;
         }
-        return ans;
+       
+        return  ans;
     }
 };
