cacfe011beabd5e7e4c98797073b22a46bb4ecec
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-02-20 05:53:42 +0530

    Time: 20 ms (95.61%), Space: 11.4 MB (59.43%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -23,10 +23,39 @@ class Solution {
     }
     
     
+    int solveOptimised(vector<vector<int>> &intervals) {
+        
+        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b){
+           
+            if(a[0] == b[0])
+                return a[1] > b[1];
+            
+            return a[0] < b[0];
+            
+        });
+        
+        int n = intervals.size();
+        int maxCoveredInRight = 0;
+        int coveredIntervals = 0;
+        
+        for(int i = 0; i < n; i++) {
+            
+            bool isCovered = maxCoveredInRight >= intervals[i][1];
+            
+            maxCoveredInRight = max(maxCoveredInRight, intervals[i][1]);
+            
+            coveredIntervals += isCovered;
+        }
+        
+        return n - coveredIntervals;
+    }
+    
+    
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         
-        int ans = solveBruteForce(intervals);
+        // int ans = solveBruteForce(intervals);
+        int ans = solveOptimised(intervals);
         
         return ans;
         
