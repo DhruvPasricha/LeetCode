a34cf6f1647a77801896d8ef48feafec7ae063a0
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-02-22 08:01:18 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -11,7 +11,7 @@ class Solution{
     
     int dp[101][101];
     
-    int solve(vector<pair<int,int>> &A, int left, int right) {
+    int solve(int A[], int left, int right) {
         
         if(left >= right) {
             return 0;
@@ -29,7 +29,7 @@ class Solution{
             int leftAns = solve(A, left, k);
             int rightAns = solve(A, k + 1, right);
             
-            int cur = leftAns + rightAns + (A[left].first * A[k].second * A[right].second);
+            int cur = leftAns + rightAns + (A[left] * A[k + 1] * A[right + 1]);
             
             ans = min(ans, cur);
             
@@ -45,18 +45,10 @@ class Solution{
 public:
     int matrixMultiplication(int N, int arr[])
     {
-        // code here
-        
-        vector<pair<int,int>> matrices(N - 1);
-        
-        for(int i = 0; i < N - 1; i++) {
-            matrices[i].first = arr[i];
-            matrices[i].second = arr[i + 1];
-        }
-        
+
         memset(dp, -1, sizeof(dp));
         
-        int ans = solve(matrices, 0, N - 2);
+        int ans = solve(arr, 0, N - 2);
         
         return ans;
     }
