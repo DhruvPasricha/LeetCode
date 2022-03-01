8a4e5451b024feeef1b69369eeec264be39e220b
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-03-01 15:41:44 +0530

    Time: 3 ms (35.86%), Space: 6.3 MB (51.26%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        map<char, char> mp;
+        
+        mp[')'] = '(';
+        mp[']'] = '[';
+        mp['}'] = '{';
+        
+        stack<char> st;
+        for(char ch : s) {
+            
+            if(mp.count(ch) == 0) {
+                st.push(ch);
+            } else {
+                
+                if(st.size() == 0 or st.top() != mp[ch])
+                    return false;
+                
+                st.pop();
+                
+            }
+        }
+            
+        return st.size() == 0;
+    
+    }
+};
