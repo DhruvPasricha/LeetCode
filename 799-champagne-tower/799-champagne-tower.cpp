ffa31b3610e4e46fac39faa54781d986f9045b2f
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-03-04 06:08:31 +0530

    Time: 25 ms (32.66%), Space: 14 MB (56.71%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -3,11 +3,11 @@ public:
     double champagneTower(int poured, int query_row, int query_glass) {
         
         int n = query_row + 1;
+        
         vector<vector<long double>> dp(n + 1);
         
-        for(int i = 0;i < n;i++) {
+        for(int i = 0; i < n; i++) 
             dp[i].resize(i + 1);
-        }
         
         dp[0][0] = poured;
         
@@ -16,16 +16,23 @@ public:
             
             int m = i + 1;
             
-            for(int j = 0;j < m;j++) {
+            for(int j = 0; j < m; j++) {
                 
                 if(j < m - 1)
-                    dp[i][j] = (dp[i-1][j] >= 1) ? (dp[i-1][j] - 1) / 2 : 0;
+                    dp[i][j] = (dp[i - 1][j] >= 1) ? (dp[i - 1][j] - 1) / 2 : 0;
                 
                 if(j - 1 >= 0) 
                     dp[i][j] += (dp[i - 1][j - 1] >= 1) ? (dp[i - 1][j - 1] - 1) / 2 : 0;
             }
         }
         
-        return min((long double)1, dp[query_row][query_glass]);
+        
+        for(int i = 0; i < n; i++) {
+            for(auto &x : dp[i]) {
+                x = min((long double)1, x);
+            }
+        }
+        
+        return dp[query_row][query_glass];
     }
 };
