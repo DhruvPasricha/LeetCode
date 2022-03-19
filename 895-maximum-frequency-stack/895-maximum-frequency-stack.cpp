cf7e7ab221a5806722c62fe340b9365e4fa4ca38
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-03-19 15:00:05 +0530

    Time: 418 ms (15.96%), Space: 104.1 MB (11.45%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -1,91 +1,32 @@
-class Node{
- public:
-    Node *prev;
-    int val;
-    Node *next;
-    Node *prevSame;
-    int index;
-    
-    Node(int v, int t) {
-        
-        val = v;
-        prev = next = prevSame = NULL;
-        index = t;
-    }
-};
-
 class FreqStack {
     
+    const int N = 2*1e4 + 10;
     
-    Node *head;
-    Node *tail;
-    
-    unordered_map<int, Node*> topMostAddress;
-    
-    map<int, set<pair<int,int>, greater<pair<int,int>>>, greater<int>> mp; // {freq -> {index, val}}
-    
-    unordered_map<int, int> freq; // freq
-    
-    int t = 0;
+    vector<stack<int>> st;
+    unordered_map<int, int> freq;
+    int mx = 0;
     
 public:
     FreqStack() {
-        
-        head = tail = new Node(INT_MIN, ++t); // dummy head
+        st.resize(1);
     }
     
     void push(int val) {
-        
-        auto temp = new Node(val, ++t);
-        
-        if(freq[val] > 0) {
-            temp -> prevSame = topMostAddress[val];
-            auto itr = mp[freq[val]].find({temp -> prevSame -> index, val});
-            mp[freq[val]].erase(itr);
-        }
-        
         freq[val]++;
-        
-        tail -> next = temp;
-        temp -> prev = tail;
-        tail = temp;
-        
-        topMostAddress[val] = temp;
-        
-        mp[freq[val]].insert({t, val});
-        
+        if(freq[val] == st.size()) {
+            st.push_back({});
+        }
+        st[freq[val]].push(val);
+        mx = max(freq[val], mx);
     }
     
     int pop() {
-        
-        
-        auto &st = mp.begin()->second;
-        
-        int val = st.begin()->second;
-        
-        auto temp = topMostAddress[val];
-        
-        // delete temp
-        temp -> prev -> next = temp -> next;
-        
-        if(temp -> next)
-          temp -> next -> prev = temp -> prev;
-        else
-            tail = temp -> prev;
-        
-        mp[freq[val]].erase({temp->index, val});
-        
-         
-        if(mp[freq[val]].size() == 0)
-            mp.erase(freq[val]);
-        
+        int val = st[mx].top();
+        st[mx].pop();
         freq[val]--;
-        
-        if(freq[val]) {
-            topMostAddress[val] = temp -> prevSame;
-            mp[freq[val]].insert({topMostAddress[val]->index, val});
-        }
-        
+        if(st[mx].size() == 0) 
+            mx--;
         return val;
     }
 };
+
