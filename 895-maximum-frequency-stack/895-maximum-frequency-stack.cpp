8bb9eaa19368ee711414670af788e537ec46451f
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-03-19 15:01:14 +0530

    Time: 172 ms (92.40%), Space: 104.5 MB (11.36%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -1,31 +1,37 @@
 class FreqStack {
     
-    const int N = 2*1e4 + 10;
-    
     vector<stack<int>> st;
-    unordered_map<int, int> freq;
+    map<int, int> freq;
     int mx = 0;
     
 public:
+    
     FreqStack() {
         st.resize(1);
     }
     
     void push(int val) {
+        
         freq[val]++;
-        if(freq[val] == st.size()) {
+        
+        if(freq[val] == st.size())
             st.push_back({});
-        }
+        
         st[freq[val]].push(val);
+        
         mx = max(freq[val], mx);
     }
     
     int pop() {
+        
         int val = st[mx].top();
+        
         st[mx].pop();
+        
         freq[val]--;
-        if(st[mx].size() == 0) 
-            mx--;
+        
+        if(st[mx].size() == 0)  mx--;
+        
         return val;
     }
 };
