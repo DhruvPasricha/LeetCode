f18904d796c01b4240e2f070e7df47023d692f46
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-03-19 16:51:40 +0530

    Time: 7 ms (62.57%), Space: 16.6 MB (10.80%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -1,30 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    
-    bool solve(TreeNode* root1, TreeNode* root2) {
-        
-        if(root1 == NULL and root2 == NULL)
-            return true;
-        
-        if(root1 == NULL or root2 == NULL)
-            return false;
-        
-        return root1 -> val == root2 -> val and solve(root1->right, root2->left) and solve(root1->left, root2->right); 
-        
-    }
     
 public:
     bool isSymmetric(TreeNode* root) {   
-        return solve(root -> left, root -> right);
+        
+        stack<pair<TreeNode*, TreeNode*>> st;
+        st.push({root->left, root->right});
+        
+        
+        while(!st.empty()) {
+            
+            auto [root1, root2] = st.top();
+            
+            st.pop();
+            
+            if(root1 == NULL and root2 == NULL)
+                continue;
+        
+            if(root1 == NULL or root2 == NULL)
+                return false;
+            
+            if(root1 -> val != root2 -> val)
+                return false;
+            
+            st.push({root1->right, root2->left});
+            st.push({root1->left, root2->right});
+            
+        }
+        
+        return true;
     }
 };
