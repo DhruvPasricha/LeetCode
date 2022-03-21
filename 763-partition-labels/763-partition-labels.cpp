521dd496867aeede07deade94196fccc241346c4
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-03-21 22:20:18 +0530

    Time: 7 ms (41.00%), Space: 6.6 MB (64.08%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,61 +1,30 @@
 class Solution {
-    
-    vector<int> lengthOfMergedIntervals(vector<pair<int,int>> &intervals) {
-        
-        vector<int> Ans;
-        
-        int i = 0;
-        int n = intervals.size();
-        
-        while(i < n) {
-            
-            auto [l, r] = intervals[i];
-            
-            while(i < n and intervals[i].first <= r) {
-                r = max(r, intervals[i].second);
-                i++;
-            }
-            
-            Ans.push_back(r - l + 1);
-            
-        }
-        
-        return Ans;
-        
-    }
-    
 public:
     vector<int> partitionLabels(string s) {
         
-        vector<int> firstOccurence(26, INT_MAX);
-        vector<int> lastOccurence(26, INT_MIN);
+        vector<int> lastOccurence(26);
         
         int n = s.size();
         
+        for(int i = 0; i < n; i++)
+            lastOccurence[s[i] - 'a'] = i;
+        
+        
+        int curLen = 0;
+        int curMaxIndex = 0;
+        vector<int> Ans;
+        
         for(int i = 0; i < n; i++) {
             
-            auto &first = firstOccurence[s[i] - 'a'];
-            auto &last = lastOccurence[s[i] - 'a'];
+            curLen++;
+            curMaxIndex = max(curMaxIndex, lastOccurence[s[i] - 'a']);
             
-            first = min(first, i);
-            last = max(last, i);
+            if(curMaxIndex == i) {
+                Ans.push_back(curLen);
+                curLen = curMaxIndex = 0;
+            }
         }
         
-        vector<pair<int,int>> intervals;
-        
-        for(int i = 0; i < 26; i++) {
-            
-            auto &first = firstOccurence[i];
-            auto &last = lastOccurence[i];
-            
-            if(first == INT_MAX) continue;
-            
-            intervals.push_back({first, last});
-        }
-        
-        sort(intervals.begin(), intervals.end());
-        
-        return lengthOfMergedIntervals(intervals);
+        return Ans;
     }
 };
-
