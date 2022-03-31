d9b7acfb55b7fef79a7d11bfccaff2baae10e084
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-03-31 16:21:10 +0530

    Time: 7 ms (35.73%), Space: 7.1 MB (74.76%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,39 +1,42 @@
 class Solution {
     
-    bool good(vector<int> &A, int k, int mxSum) {
+    
+    bool good(vector<int> &A, int k, int x) {
         
         
         int cur = 0;
+        int n = A.size();
         
-        for(int &x : A) {
-            if(cur + x <= mxSum) {
-                cur += x;
-            } else if(x > mxSum){
-                return false;
-            }else{
-                cur = x;
+        for(int i = 0; i < n; i++) {
+            
+            if(cur + A[i] > x) {
+                cur = 0;
                 k--;
             }
+            
+            cur += A[i];
+            
+            if(cur > x) return false;
+            
         }
         
-        return k > 0;
-    
+        return k >= 0;
+        
     }
     
     
 public:
     int splitArray(vector<int>& nums, int k) {
         
-        int64_t l = - 1;
-        int64_t r = INT_MAX;
+        int l = -1;
+        int r = 1e9;
         
         
         while(r - l > 1) {
             
-            int64_t m = (l + r) / 2;
+            int m = l + (r - l) / 2;
             
-            
-            if(good(nums, k, m))
+            if(good(nums, k - 1, m))
                 r = m;
             else
                 l = m;
