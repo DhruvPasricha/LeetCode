c722517a1ef765a36a55100a18704b7b68bd6bc9
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-04-06 05:24:12 +0530

    Time: 111 ms (66.29%), Space: 58.9 MB (81.59%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,58 +4,22 @@ public:
     int maxArea(vector<int> &height)
     {
 
-        int n = height.size();
-        queue<pair<int, int>> left, right;
-
-        pair<int, int> prev = {INT_MIN, INT_MIN};
-
-        for (int i = 0; i < n; i++)
-        {
-
-            if (height[i] > prev.first)
-            {
-                left.push({height[i], i});
-                prev = left.front();
-            }
-        }
-
-        prev.first = prev.second = INT_MIN;
-
-        for (int i = n - 1; i >= 0; i--)
-        {
-
-            if (height[i] > prev.first)
-            {
-                right.push({height[i], i});
-                prev = right.front();
-            }
-        }
-
+        int i = 0;
+        int j = height.size() - 1;
         int ans = 0;
-
-        int h = min(left.front().first, right.front().first);
-
-        while (left.size() > 0 and right.size() > 0)
-        {
-
-            while (left.size() > 0 and left.front().first < h)
-                left.pop();
-
-            while (right.size() > 0 and right.front().first < h)
-                right.pop();
-
-            if (left.size() == 0 or right.size() == 0)
-                return ans;
-
-            h = min(left.front().first, right.front().first);
-
-            int cur = h * (right.front().second - left.front().second);
-
+        
+        while(i < j) {
+            
+            int cur = (j - i) * min(height[j], height[i]);
             ans = max(ans, cur);
-
-            h++;
+            
+            if(height[i] <= height[j])
+                i++;
+            else
+                j--;
+            
         }
-
+        
         return ans;
     }
 };
