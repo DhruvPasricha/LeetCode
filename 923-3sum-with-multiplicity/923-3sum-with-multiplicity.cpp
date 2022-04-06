2844a84207459872333c13006f99eb70dd8fb008
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-04-06 06:09:30 +0530

    Time: 1190 ms (23.47%), Space: 42.4 MB (5.44%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,22 +1,7 @@
 class Solution {
     
     const int64_t mod = 1e9 + 7;
-    int64_t dp[1002][301][4];
-    
-    
-    int64_t nCr(int n, int r) {
-        
-        int64_t num = 1;
-        int64_t den = 1;
-        
-        for(int64_t i = 0; i < r; i++) {
-            num *= (n - i);
-            den *= (i + 1);
-        }
-        
-        return num / den;
-        
-    }
+    int64_t dp[3001][301][4];
     
     int64_t solve(vector<int> &A, int n, int target, int req) {
         
@@ -30,36 +15,19 @@ class Solution {
         
         if(ans != -1)
             return ans;
-            
-        int mx = min(A[n - 1], req);
-        int curSum = 0;
-        ans = 0;
         
-        for(int i = 0;i <= mx; i++) {
-            auto cur = solve(A, n - 1, target - curSum, req - i);     
-            cur *= nCr(A[n - 1], i);  
-            cur %= mod;
-            ans += cur;
-            ans %= mod;
-            curSum += n - 1;
-        }
+        int64_t ans1 = solve(A, n - 1, target, req);
+        int64_t ans2 = (req) ? solve(A, n - 1, target - A[n-1], req - 1) : 0;
         
-        return ans;
-        
+        return ans = (ans1 + ans2) % mod;
     }
     
 public:
     int threeSumMulti(vector<int>& arr, int target) {
         
-        vector<int> freq(101);
-        
-        for(int &x : arr)
-            freq[x]++;
-        
         memset(dp, -1, sizeof(dp));
         
-        int ans = solve(freq, 101, target, 3);
-        
+        int ans = solve(arr, arr.size(), target, 3);
         return ans;
     }
 };
