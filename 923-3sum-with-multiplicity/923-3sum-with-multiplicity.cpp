d89c230f49ff0606f4985d1f6869d38a2701af68
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-04-07 18:46:24 +0530

    Time: 223 ms (61.51%), Space: 10.6 MB (62.54%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,67 +1,48 @@
 class Solution {
     
     const int64_t mod = 1e9 + 7;
+    map<int,int> mp;
     
-    int64_t countPairs(vector<int> &arr, int start, int target) {
+    int64_t twoSum(int target) {
         
-        int end = arr.size() - 1;
         int64_t ans = 0;
         
-        while(start < end) {
+        for(auto &it : mp) {
             
-            int sum = arr[start] + arr[end];
+            int cur = it.first;
+            int rem = target - cur;
             
-            if(sum == target) {
-                
-                if(arr[start] == arr[end]) {
-                    
-                    int64_t f = end - start + 1;
-                    ans = (ans + (f * (f - 1)) / 2) % mod;
-                    return ans;
-                }
-                
-                int64_t f1 = 0;
-                int64_t f2 = 0;
-                
-                int start_ = start;
-                int end_ = end;
-                
-                while(start_ < end and arr[start] == arr[start_])
-                    f1++, start_++;
-                
-                while(end_ > start and arr[end] == arr[end_])
-                    f2++, end_--;
-                
-                ans = (ans + f1 * f2) % mod;
-                
-                start = start_;
-                end = end_;
-                
-            } else if(sum > target) {
-                
-                end--;
-                
-            } else {
-                
-                start++;
+            if(cur == rem)
+                continue;
+            
+            if(mp.count(rem)) {
+                ans = (ans + (int64_t)it.second * (int64_t)mp[rem]) % mod; 
             }
+        }
+        
+        ans /= 2;
+        
+        if(target % 2 == 0 and mp.count(target / 2)) {
             
+            int64_t f = mp[target / 2];
+            
+            ans = (ans + (f * (f - 1)) / 2) % mod;
         }
         
         return ans;
+        
     }
-    
     
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        
-        sort(arr.begin(), arr.end());
         
         int64_t ans = 0;
         int n = arr.size();
         
         for(int i = 0; i < n; i++) {
-            ans = (ans + countPairs(arr, i + 1, target - arr[i])) % mod;
+            ans += twoSum(target - arr[i]);
+            ans %= mod;
+            mp[arr[i]]++;
         }
         
         return ans;
