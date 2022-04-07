91934aee6040e37d091a610cf50be1e6fbddfa6e
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-04-07 18:35:56 +0530

    Time: 249 ms (59.11%), Space: 10.5 MB (74.57%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,33 +1,69 @@
 class Solution {
     
     const int64_t mod = 1e9 + 7;
-    int64_t dp[3001][301][4];
     
-    int64_t solve(vector<int> &A, int n, int target, int req) {
+    int64_t countPairs(vector<int> &arr, int start, int target) {
         
-        if(target < 0)
-            return 0;
+        int end = arr.size() - 1;
+        int64_t ans = 0;
         
-        if(n == 0)
-            return target == 0 and req == 0;
+        while(start < end) {
+            
+            int sum = arr[start] + arr[end];
+            
+            if(sum == target) {
+                
+                if(arr[start] == arr[end]) {
+                    
+                    int64_t f = end - start + 1;
+                    ans = (ans + (f * (f - 1)) / 2) % mod;
+                    return ans;
+                }
+                
+                int64_t f1 = 0;
+                int64_t f2 = 0;
+                
+                int start_ = start;
+                int end_ = end;
+                
+                while(start_ < end and arr[start] == arr[start_])
+                    f1++, start_++;
+                
+                while(end_ > start and arr[end] == arr[end_])
+                    f2++, end_--;
+                
+                ans = (ans + f1 * f2) % mod;
+                
+                start = start_;
+                end = end_;
+                
+            } else if(sum > target) {
+                
+                end--;
+                
+            } else {
+                
+                start++;
+            }
+            
+        }
         
-        auto &ans = dp[n][target][req];
-        
-        if(ans != -1)
-            return ans;
-        
-        int64_t ans1 = solve(A, n - 1, target, req);
-        int64_t ans2 = (req) ? solve(A, n - 1, target - A[n-1], req - 1) : 0;
-        
-        return ans = (ans1 + ans2) % mod;
+        return ans;
     }
+    
     
 public:
     int threeSumMulti(vector<int>& arr, int target) {
         
-        memset(dp, -1, sizeof(dp));
+        sort(arr.begin(), arr.end());
         
-        int ans = solve(arr, arr.size(), target, 3);
+        int64_t ans = 0;
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++) {
+            ans = (ans + countPairs(arr, i + 1, target - arr[i])) % mod;
+        }
+        
         return ans;
     }
 };
