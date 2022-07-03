93b3230b3f503377855c20598d555f09da46c80c
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-07-03 06:12:28 +0530

    Time: 0 ms (100.00%), Space: 7 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,22 +1,15 @@
 class Solution {
-    int solve(vector<int> &A, int val, bool isDecreasing) {
-        int ans = 1;
-        for(int &x : A) {
-            if((isDecreasing and x > val) or ((not isDecreasing) and x < val)) {
-                ++ans;
-                isDecreasing = not isDecreasing;
-            }
-            val = x;
-        }
-        return ans;
-    }
 public:
     int wiggleMaxLength(vector<int>& nums) {        
-        int val = nums[0];
-        bool isDecreasing = true;
-        int ans1 = solve(nums, val, isDecreasing);
-        isDecreasing = false;
-        int ans2 = solve(nums, val, isDecreasing);
-        return max(ans1, ans2);
+        int n = nums.size();
+        int up = 1, down = 1;
+        for(int i = 1; i < n; ++i) {
+            if(nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if(nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return max(up, down);
     }
 };
