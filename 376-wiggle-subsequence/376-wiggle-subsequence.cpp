dbfba7e78301d20fc355062e6c41c1d1e8f4b8c0
Dhruv Pasricha <78498002+DhruvPasricha@users.noreply.github.com>
2022-07-03 06:00:27 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (19.43%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,24 +1,21 @@
 class Solution {
-public:
-    int wiggleMaxLength(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2));
-        int ans = 0;
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < 2; ++j) {
-                bool isSmallerNeeded = (j == 0);
-                for(int k = i - 1; k >= 0; --k) {
-                    if(nums[i] == nums[k]) continue;
-                    if(isSmallerNeeded and nums[k] < nums[i]) {
-                        dp[i][j] = max(dp[i][j], dp[k][1]);
-                    } else if(not isSmallerNeeded and nums[k] > nums[i]) {
-                        dp[i][j] = max(dp[i][j], dp[k][0]);
-                    }
-                }
-                dp[i][j]++;
-                ans = max(ans, dp[i][j]);
+    void solve(vector<int> &A, stack<pair<int, bool>> &s) {
+        for(int &x : A) {
+            auto [val, isDecreasing] = s.top();
+            if((isDecreasing and x > val) or ((not isDecreasing) and x < val)) {
+                s.push({x, not isDecreasing});
+            } else {
+                s.top().first = x;
             }
         }
-        return ans;
+    }
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        stack<pair<int, bool>> s1, s2;
+        s1.push({nums[0], true});
+        s2.push({nums[0], false});        
+        solve(nums, s1);
+        solve(nums, s2);
+        return max(s1.size(), s2.size());
     }
 };
